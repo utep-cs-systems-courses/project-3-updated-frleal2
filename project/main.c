73b72f54f34d26d9de735133da75ff044838b420
Felipe <frleal2@miners.utep.edu>
2020-11-29 15:23:06 -0700

    added mario pixel art

--- project/main.c
+++ project/main.c
@@ -5,12 +5,11 @@
 #include "buzzer.h"
 
 int main(){
+  state_init();
   configureClocks();
-  
   switch_init();
   led_init();
   buzzer_init();
-  state_init();
   enableWDTInterrupts();
   
   or_sr(0x18);
